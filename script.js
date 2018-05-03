47c7882f4ce7196d6dd2ca73cbfbe39fbe38a49f
Molly Brown <molly.stra@gmail.com>
2018-05-03 16:32:36 -0400

    Invalid or Valid class added to form items on submit. Forgotten </div> added to HTML.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Add validation!');
+function validateForm (fields) {
+    document.querySelectorAll("input").forEach(function (fields) {
+        var val = fields.value
+        if (val == '') {
+            fields.parentElement.classList.add("input-invalid")
+        } else {
+            fields.parentElement.classList.add("input-valid")
+        }
+})
+}
+
+
+  function run() {
+
+  document.querySelector("#submit-button").addEventListener('click', function (event) {
+    validateForm()
+    event.preventDefault()
+  })
+}
+
+document.addEventListener('DOMContentLoaded', run());
