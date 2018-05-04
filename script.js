af892669cd0df812141a7cb285ad8c7e1001c0a2
Molly Brown <molly.stra@gmail.com>
2018-05-03 20:16:11 -0400

    Required message will disappear after submitting valid form. Still need to prevent repeat error messages.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 function validateForm (fields) {
     document.querySelectorAll("input").forEach(function (fields) {
         var val = fields.value
@@ -6,30 +9,38 @@ function validateForm (fields) {
             var reg = fields.previousElementSibling.innerText
             var newDiv = document.createElement("div")
             var newText = document.createTextNode(reg + " is required")
-            // newDiv.classList.add("input-invalid")
+            newDiv.setAttribute("id",reg)
             if (val == '') {
                 par.classList.add("input-invalid")
                 newDiv.appendChild(newText)
                 par.appendChild(newDiv)               
             } else {
                 par.classList.add("input-valid")
+                var handle = document.getElementById(reg)
+                console.log(handle)
+                if (handle !== null) {
+                    handle.remove()
+                }
             }
         } else {
             var regCar = fields.placeholder
             var newDiv = document.createElement("div")
             var newText = document.createTextNode(regCar + " is required")
-            // newDiv.classList.add("input-invalid")
+            newDiv.setAttribute("id",regCar)
             if (val == '') {
                 par.parentElement.classList.add("input-invalid")
                 newDiv.appendChild(newText)
                 par.parentElement.appendChild(newDiv) 
             } else {
                 par.parentElement.classList.add("input-valid")
-                
+                var handleCar = document.getElementById(regCar)
+                console.log(handle)
+                if (handleCar !== null) {
+                    handleCar.remove()
+                }
             }
-        }
-})
-}
+        }}
+)}
 
   function run() {
 
